605bf68b1f1e540af373d62577e4632ee64e1f7b
AlbertMakdissi <albert.makdisi2000@gmail.com>
2022-03-19 16:57:02 +0100

    changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const userData = document.querySelector("[data-user]");
 const userDataCards = document.querySelector("[data-user-cards]");
 const searchField = document.querySelector("[data-search]");
-
+let users = [];
 //option 1//
 /* fetch("https://jsonplaceholder.typicode.com/users")
   .then((res) => res.json())
@@ -19,11 +19,23 @@ const searchField = document.querySelector("[data-search]");
     });
   }); */
 
+searchField.addEventListener("input", (event) => {
+  const inputValue = event.target.value.toLowerCase();
+  users.forEach((user) => {
+    const isVisible =
+      user.name.toLowerCase().includes(inputValue) ||
+      user.email.toLowerCase().includes(inputValue) ||
+      user.username.toLowerCase().includes(inputValue);
+    user.element.classList.toggle("hide", !isVisible);
+  });
+});
+
+//option2 ajax approach
 const url = "https://jsonplaceholder.typicode.com/users";
 async function getUserData() {
   const response = await fetch(url);
   const data = await response.json();
-  data.forEach((user) => {
+  users = data.map((user) => {
     const card = userData.content.cloneNode(true).children[0];
     const header = card.querySelector("[data-header]");
     const body = card.querySelector("[data-body]");
@@ -33,6 +45,7 @@ async function getUserData() {
     body.textContent = email;
     footer.textContent = username;
     userDataCards.append(card);
+    return { name: name, email: email, username: username, element: card };
   });
 }
 
